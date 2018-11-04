11fb675685bdde9bd250c83510f34adefc7f8d58
cheng <792702352@qq.com>
2018-11-04 14:06:04 +0800

    :sparkles: 代理模式: 模版方法模式

---
+++ src/main/java/com/cheng/design/behavioral/templetemethod/FECourse.java
@@ -0,0 +1,28 @@
+package com.cheng.design.behavioral.templetemethod;
+
+/**
+ * @author cheng
+ *         2018/11/4 13:44
+ */
+public class FECourse extends AbstractCourse {
+
+    private boolean needWriteArticleFlag = false;
+
+    public FECourse() {
+    }
+
+    public FECourse(boolean needWriteArticleFlag) {
+        this.needWriteArticleFlag = needWriteArticleFlag;
+    }
+
+    @Override
+    void packageCourse() {
+        System.out.println("提供课程的前端源代码");
+        System.out.println("提供课程的图片等多媒体素材");
+    }
+
+    @Override
+    protected boolean needWriteArticle() {
+        return needWriteArticleFlag;
+    }
+}
---
+++ src/main/java/com/cheng/design/behavioral/templetemethod/Test.java
@@ -0,0 +1,22 @@
+package com.cheng.design.behavioral.templetemethod;
+
+/**
+ * 模版方法模式
+ *
+ * @author cheng
+ *         2018/11/4 13:48
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("后端设计模式课程 start ---");
+        AbstractCourse designPatternCourse = new DesignPatternAbstractCourse();
+        designPatternCourse.makeCourse();
+        System.out.println("后端设计模式课程  end  ---");
+
+        System.out.println("前端课程 start ---");
+        AbstractCourse vueCourse = new FECourse(true);
+        vueCourse.makeCourse();
+        System.out.println("前端课程  end  ---");
+    }
+}
