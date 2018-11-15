c0c0250024ddfea0921604c95227b40cf49a9d00
cheng <792702352@qq.com>
2018-11-15 15:35:57 +0800

    :sparkles: 设计模式: 中介者模式

---
+++ src/main/java/com/cheng/design/behavioral/mediator/StudyGroup.java
@@ -0,0 +1,14 @@
+package com.cheng.design.behavioral.mediator;
+
+import java.util.Date;
+
+/**
+ * @author cheng
+ *         2018/11/15 15:31
+ */
+public class StudyGroup {
+
+    public static void showMessage(User user, String message) {
+        System.out.println(new Date().toString() + " [" + user.getName() + "] : " + message);
+    }
+}
---
+++ src/main/java/com/cheng/design/behavioral/mediator/User.java
@@ -0,0 +1,26 @@
+package com.cheng.design.behavioral.mediator;
+
+/**
+ * @author cheng
+ *         2018/11/15 15:31
+ */
+public class User {
+
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public void sendMessage(String message) {
+        StudyGroup.showMessage(this, message);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
